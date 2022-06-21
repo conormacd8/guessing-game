7e8a08ef0ba35ab55d447f0400e0156580e2f46d
Conor MacDonald <conormacd8@gmail.com>
2022-06-21 17:30:45 +1000

    Card cant be clicked again
    Made sure that card click won't be processed when clicked if already clicked or if already matched

--- index.js
+++ index.js
@@ -8,22 +8,39 @@ for (let i = cardContainer.children.length; i >= 0; i--) {
 }
 
 Array.from(cardContainer.children).forEach(card => {
-  card.addEventListener('click', () => {
-      if (previousCard === undefined){
-        previousCard = card.dataset.card
-        console.log(previousCard)
-      }
-      else {
-        let currentCard = card.dataset.card
-        if (currentCard == previousCard){
-          alert("Matching Pair, well done!")
+    card.addEventListener('click', () => {
+      if (!card.classList.contains('cursor-not-allowed')){
+        if (previousCard === undefined){
+          previousCard = card.dataset.card
+          card.classList.add('cursor-not-allowed')
+          console.log(previousCard)
         }
-        else{
-          previousCard = ''
-          currentCard = ''
-          alert("Not a match")
-          failureCount += 1
+        else {
+          let currentCard = card.dataset.card
+          if (currentCard == previousCard){
+            alert("Matching Pair, well done!")
+            Array.from(document.querySelectorAll(`[data-card="${currentCard}"]`)).forEach(card => {
+              card.classList.add('cursor-not-allowed')
+            })
+            successCount += 1
+              if (successCount === cardContainer.children.length /2){
+                alert("Congratulations! You matched all the cards!")
+              }
+          }
+          else{
+            alert("Not a match")
+            failureCount += 1
+            Array.from(document.querySelectorAll(`[data-card="${previousCard}"]`)).forEach(card => {
+              card.classList.remove('cursor-not-allowed')
+            })
+          }
+          previousCard = undefined
+          currentCard = undefined
         }
+        console.log(successCount)
+
       }
-    })
+      }
+      )
+
 })
