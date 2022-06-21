8be6b7f6eb0f049bebd7b388d603595b7095e30d
Conor MacDonald <conormacd8@gmail.com>
2022-06-21 14:32:41 +1000

    Added styling and html layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'cards': 'repeat(auto-fill, minmax(208px, 1fr))',
+      },
+    },
   },
   plugins: [],
 }
